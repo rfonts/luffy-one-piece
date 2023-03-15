f5366a54c2976dbd347934e31dea9b4ac7c6a246
Rodrigo Fontes <58378621+rfonts@users.noreply.github.com>
2023-03-14 23:29:45 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem')
+
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+
+        desativarBotaoSelecionado();
+
+        selecionarBotaoCarrosel(botao);
+
+        esconderImagemAtiva();
+
+        mostrarImagemDeFundo(indice);
+    })
+})
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
+
+function selecionarBotaoCarrosel(botao) {
+    botao.classList.add('selecionado');
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
+
+function mostrarImagemDeFundo(indice) {
+    imagens[indice].classList.add('ativa')
+}
